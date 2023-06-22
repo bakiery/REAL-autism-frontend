0a4e1b8ff8facd903cd7a6237adbaf2e1f603ea2
razanne bacchus <bacchusr@protonmail.com>
2023-06-22 11:14:02 +0100

    done final_v3

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import streamlit as st
 import requests
 from PIL import Image
 import os
-from streamlit_extras.colored_header import colored_header
 
 
 # Set the API endpoint URL
@@ -54,12 +53,9 @@ def main():
     st.sidebar.image(logo_image_resized)
 
     # Display the title in the main section
-    colored_header(
-    label="Is My Child Autistic?",
-    description="Find out the possibility of your child having autism with AI facial detection",
-    color_name="violet-70",
-)
-
+    st.write('<h2 style="color: #F74F9C;"> Is My Child Autistic? </h2>', unsafe_allow_html=True)
+    st.markdown("<div style='background-color: #2E86EF; height: 5px'></div>", unsafe_allow_html = True)
+    st.write ('Find out the possibility of your child having autism with AI facial detection')
     # Check if the 'styles.css' file exists
     css_file_path = os.path.join(os.path.dirname(__file__), 'styles.css')
     if not os.path.isfile(css_file_path):
@@ -74,24 +70,6 @@ def main():
     page = st.sidebar.selectbox('Navigate', options=pages)
 
     if page == 'Home':
-        # Display the introduction and instructions
-        st.markdown('''
-            ## AI Facial Assessment Tool
-
-    This tool is designed to provide information about facial features associated with Autism Spectrum Disorder (ASD). Please follow the instructions below to use the tool effectively:
-
-    1. Upload a photo: Click on the "Upload Photo" button to select and upload a photo of the individual whose facial features you would like to analyze.
-
-    2. Analyze facial features: Once the photo is uploaded, the tool will analyze the facial features based on research findings related to ASD. The analysis may take a few moments.
-
-    3. Interpretation: After the analysis is complete, the tool will provide you with the likelihood of the child having autism based on their facial landmarks, compared against faces from the trained datasets of other autistic and non-autistic children's faces. Please remember that this tool is for informational purposes only and should not be used as a diagnostic tool. A formal diagnosis of autism should be made by a qualified healthcare professional.
-
-    Interpretation of Scores:
-    - Score below 30%: Indicates a lower likelihood of ASD-related facial features.
-    - Score between 30% to 70%: Suggests a moderate likelihood of ASD-related facial features.
-    - Score above 70%: Indicates a higher likelihood of ASD-related facial features.
-
-            ''')
 
         # Upload image
         uploaded_file = st.file_uploader('Upload an image', type=['jpg', 'jpeg', 'png'])
@@ -106,10 +84,24 @@ def main():
             probability_percentage = round(result["prediction"][0][0] * 100, 2)
             st.subheader(f'Probability of Autism: {probability_percentage}%')
 
+        st.write('<h3 style="color: #2E86EF;"> Interpretation of Scores </h3>', unsafe_allow_html=True)
+        st.markdown('''
+    - **Score below 30%:** <br> Indicates a lower likelihood of ASD-related facial features.
+    - **Score between 30% to 70%:**<br> Suggests a moderate likelihood of ASD-related facial features.
+    - **Score above 70%:**<br> Indicates a higher likelihood of ASD-related facial features.
+''', unsafe_allow_html=True)
+
+        st.write('<h3 style="color: #2E86EF;"> Background </h3>', unsafe_allow_html=True)
+        st.markdown('''
+    This tool is designed to provide information about facial features associated with Autism Spectrum Disorder (ASD). It will analyze the facial features based on research findings related to ASD.
+
+    Please remember that this tool is for informational purposes only and should not be used as a diagnostic tool. A formal diagnosis of autism should be made by a qualified healthcare professional.
+
+            ''', unsafe_allow_html=True)
 
     elif page == 'About':
+        st.write('<h3 style="color: #2E86EF;"> Understanding Facial Features Associated with Autism Spectrum Disorder (ASD) </h3>', unsafe_allow_html=True)
         st.markdown('''
-    ## Understanding Facial Features Associated with Autism Spectrum Disorder (ASD)
     Recent studies have identified certain facial features that are commonly observed in children with Autism Spectrum Disorder (ASD). These features include a broader top face, a shorter midface, eyes with a wider palpebral fissure length (PFL) and interpupillary distance (IPD), a wider mouth, and a shorter and wider philtrum (the groove between the nose and top lip)[1].
 
     Moreover, there are suggestions from other sources that indicate a potential association between ASD and other features such as larger lips and small tufting in spots at the hairline. However, it's important to note that these features are not universally present in individuals with autism and require further verification through additional research.
@@ -119,8 +111,10 @@ def main():
     It's worth mentioning that these facial features associated with autism are generally subtle and may not be noticeably distinct to the average observer. Additionally, the presence of these features can vary within the autistic population, as autism itself encompasses a range of characteristics.
 
     Please note that this tool is for informational purposes only and should not be used as a diagnostic tool. A formal diagnosis of autism should be made by a qualified healthcare professional.
+    ''')
 
-    ### References
+    st.write('<h3 style="color: #2E86EF;"> References </h3>', unsafe_allow_html=True)
+    st.markdown('''
     For more information and references, please refer to the studies provided:
 
     - Mujeeb Rahman KK and Subashini MM. "Identification of Autism in Children Using Static Facial Features and Deep Neural Networks." *Brain Sci*, vol. 12, no. 1, 2022, article 94. doi: [10.3390/brainsci12010094](https://doi.org/10.3390/brainsci12010094). PMID: 35053837; PMCID: PMC8773918. [1]
